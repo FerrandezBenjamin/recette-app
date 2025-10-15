27b6053697eeca08c4cde01db44fe29fab202380
Benjamin <b.ferrandez@thomyris.com>
2025-10-15 15:33:11 +0200

    qql changement front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,21 @@
 
 @section('content')
 
-    <div class="container flex-center flex-col">
-        <h1>Bienvenue</h1>
-        <div class="dishes flex-arround gap-my-first">
+    <div class="container flex-center flex-col min-p">
+        <div class="container-gg">
 
-            <div class="see-dishes btn-base">
-                <a href="{{route('display_all_dishes')}}">Voir les recettes</a>
-            </div>
-            <div class="create-dishes btn-base">
-                <a href="{{route('display_new_dish')}}">Créer une recette</a>
-            </div>
 
+            <h1>Bienvenue</h1>
+            <div class="dishes gap-my-first">
+
+                <div class="see-dishes btn-base-dish">
+                    <a href="{{route('display_all_dishes')}}">Voir les recettes</a>
+                </div>
+                <div class="create-dishes btn-base-dish">
+                    <a href="{{route('display_new_dish')}}">Créer une recette</a>
+                </div>
+
+            </div>
         </div>
     </div>
 @endsection
